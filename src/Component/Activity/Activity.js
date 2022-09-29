20889954eec9737827448ff99a4e42f098916e6d
inzamam nur <inzamamnur.14@gmail.com>
2022-09-29 18:51:02 +0600

    updated

--- src/Component/Activity/Activity.js
+++ src/Component/Activity/Activity.js
@@ -5,18 +5,18 @@ const Activity = (props) => {
     const {handleClick,cards} = props;
     const {name,img,about,time,id,start} = cards
     return (
-        <div className='cards'>
+       <div className='cards-container'>
+         <div className='cards'>
             <div className="card-info">
             <img className='img' src={img} alt="" />
             <h3>{name}</h3>
             <p>{about}</p>
             <p>AT : {start}</p>
-            <h4>Spent Time : {time} <i>Minute</i></h4>
+            <h4>Time Spent: {time} <i>Minute</i></h4>
             </div>
             <button  onClick={()=>handleClick(cards)}>Add to Cart</button>
-
-    
         </div>
+       </div>
     );
 };
 
